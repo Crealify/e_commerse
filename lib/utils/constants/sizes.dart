fa58468778826877e59caa053e68efbc44ab36ba
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-10-14 16:52:47 +0545

    set constant : Color and sized  done

---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,56 @@
+class Usized {
+  Usized._();
+  // Padding and margin sizes
+  static const double xs = 4.0; // extra small
+  static const double sm = 8.0; // small
+  static const double md = 16.0; // medium
+  static const double lg = 24.0; //  large
+  static const double xl = 32.0; // extra large
+
+  // icons sizes
+  static const double iconXs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+
+  // font sizes
+  static const double fontSizeSm = 14.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontLg = 18.0;
+
+  // Button sizes
+  static const double buttonHeight = 18.0;
+  static const double buttonRadius = 12.0;
+  static const double buttonWidth = 120.0;
+
+  // App Bar sizes
+  static const double appBarHeight = 56.0;
+  
+
+  // Default Spacing between selections
+  static const double defaultSpace = 24.0;
+  static const double spaceBtwItems = 16.0;
+  static const double spaceBtwSections = 32.0;
+
+  // Border radius
+  static const double borderRadiusSm = 4.0;
+  static const double borderRadiusMd = 8.0; 
+  static const double borderRadiusLg = 16.0;  
+
+  //Product item dimesnsions
+  static const double productImageRadius = 16.0;
+
+  // Input field 
+  static const double inputFieldHeight = 12.0;
+  static const double spaceBtwInputFields = 16.0;
+
+  // Card dimensions
+  static const double cardRadiusLg = 16.0;
+  static const double cardRadiusMd = 12.0;
+  static const double cardRadiusSm = 10.0;
+  static const double cardRadiusXs = 6.0;
+
+  // Grid voew Spacing
+  static const double gridViewSpacing = 16.0;
+
+}
